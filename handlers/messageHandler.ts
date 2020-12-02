cc7b64a516004610c055289f1f8658617009e10f
lirannl <liranpiade@gmail.com>
2020-12-03 03:55:40 +1000

    Remove workarounds

--- handlers/messageHandler.ts
+++ handlers/messageHandler.ts
@@ -2,7 +2,6 @@ import BotEnv from "../botenv.ts";
 import { config } from "../deps.ts";
 import { eventHandlers, Message, cache } from "../deps.ts";
 import { nameToRole, performantMultiOr } from "../utils.ts";
-import roleCategoryHandler from "./roleCategoryHandler.ts";
 import commands from "./commands/index.ts";
 
 
@@ -18,16 +17,6 @@ const checkConstraints: (msg: Message) => boolean = (msg) => {
 export default (botEnv: BotEnv): typeof eventHandlers.messageCreate => async (msg) => {
     const guild = cache.guilds.get(config.guildID)!;
     const author = guild.members.find(member => member.user.id == msg.author.id);
-    if (msg.channelID == config.dynoChannel && msg.author.username == "Dyno" && msg.author.discriminator == "0000")
-    {
-        const eventContent = msg.embeds[0].description!;
-        if (RegExp("was (given|removed from) the").test(eventContent))
-        {
-            const member = guild.members.get(RegExp("(?<=<@!)([0-9]+)(?=>)").exec(eventContent)![0])!;
-            const role = nameToRole(guild, RegExp("(?<=the `)(.*)(?=` role)").exec(eventContent)![0])!;
-            await roleCategoryHandler(eventContent.includes("given") ? "add" : "remove")(guild, member, role.id);
-        }
-    }
     if (checkConstraints(msg) && author) {
         const command = msg.content.split(" ")[0].slice(1);
         if (commands[command]) await commands[command](botEnv, author, msg);
--- handlers/roleCategoryHandler.ts
+++ handlers/roleCategoryHandler.ts
@@ -11,17 +11,12 @@ function roleCategoryHandler(operation: "add" | "remove") {
         if (!categoryName) return; // If the role doesn't belong to any category stop running this function
         const category = nameToRole(guild, categoryName)!;
         if (operation == "add" && !member.roles.includes(category.id)) {
-            console.log(`Adding ${category.name}`);
             await addRole(guild.id, member.user.id, category.id);
         }
         else if (operation == "remove") {
             const rolesInCategory = removeFalses(config.roleCategories[categoryName].map((roleName) => nameToRole(guild, roleName)));
-            console.log(member);
-            console.log(member.roles);
             const remainingRoles = rolesInCategory.filter(role=>member.roles.includes(role.id));
-            console.log(remainingRoles);
             if (remainingRoles.length == 0 || (remainingRoles.length == 1 && remainingRoles[0].id == role.id)) {
-                console.log(`Removing ${category.name}`);
                 await removeRole(guild.id, member.user.id, category.id);
             }
         }
